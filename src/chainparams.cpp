fd722d7907885d74ce526d3c0e8f27d25dc9b916
Sebastian Rusu <sebastian.rusu@upandrunningsoftware.com>
2017-10-03 22:26:26 +0300

    Added code to mine a genesis block
    Conflicts:
    	src/chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -14,6 +14,11 @@
 
 #include "chainparamsseeds.h"
 
+// START Genesis block mining code
+// This code should be commented out once the genesis block is generated
+#include "arith_uint256.h"
+// END Genesis block mining code
+
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     CMutableTransaction txNew;
@@ -49,7 +54,6 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "09/06/2017 - Create your own avatar twin that talks like you";
->>>>>>> e740f04... Updated the pszTimestamp message
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
@@ -119,7 +123,24 @@ public:
         nDefaultPort = 8333;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1504728900, 2083236893, 0x1d00ffff, 1, 50 * COIN);
+
+        // START Genesis block mining code
+        // This code should be commented out once the genesis block is generated
+        arith_uint256 bnProofOfWorkLimit(~arith_uint256() >> 32);
+        consensus.hashGenesisBlock = uint256S("0x01");
+        if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
+            LogPrintf("recalculating params for mainnet.\n");
+            LogPrintf("old mainnet genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("old mainnet genesis hash:  %s\n", consensus.hashGenesisBlock.ToString().c_str());
+            // deliberately empty for loop finds nonce value.
+            for (genesis.nNonce = 0; UintToArith256(genesis.GetHash()) > bnProofOfWorkLimit; genesis.nNonce++) { } 
+            LogPrintf("new mainnet genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            LogPrintf("new mainnet genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("new mainnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
+        }
+        // END Genesis block mining code
+
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
@@ -218,7 +239,24 @@ public:
         nDefaultPort = 18333;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1504728900, 414098458, 0x1d00ffff, 1, 50 * COIN);
+
+        // START Genesis block mining code
+        // This code should be commented out once the genesis block is generated
+        arith_uint256 bnProofOfWorkLimit(~arith_uint256() >> 32);
+        consensus.hashGenesisBlock = uint256S("0x01");
+        if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
+            LogPrintf("recalculating params for testnet.\n");
+            LogPrintf("old testnet genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("old testnet genesis hash:  %s\n", consensus.hashGenesisBlock.ToString().c_str());
+            // deliberately empty for loop finds nonce value.
+            for (genesis.nNonce = 0; UintToArith256(genesis.GetHash()) > bnProofOfWorkLimit; genesis.nNonce++) { } 
+            LogPrintf("new testnet genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            LogPrintf("new testnet genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("new testnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
+        }
+        // END Genesis block mining code
+
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
@@ -303,7 +341,24 @@ public:
         nDefaultPort = 18444;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1504728900, 2, 0x207fffff, 1, 50 * COIN);
+
+        // START Genesis block mining code
+        // This code should be commented out once the genesis block is generated
+        arith_uint256 bnProofOfWorkLimit(~arith_uint256() >> 32);
+        consensus.hashGenesisBlock = uint256S("0x01");
+        if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
+            LogPrintf("recalculating params for regtest.\n");
+            LogPrintf("old regtest genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("old regtest genesis hash:  %s\n", consensus.hashGenesisBlock.ToString().c_str());
+            // deliberately empty for loop finds nonce value.
+            for (genesis.nNonce = 0; UintToArith256(genesis.GetHash()) > bnProofOfWorkLimit; genesis.nNonce++) { } 
+            LogPrintf("new regtest genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            LogPrintf("new regtest genesis nonce: %u\n", genesis.nNonce);
+            LogPrintf("new regtest genesis hash: %s\n", genesis.GetHash().ToString().c_str());
+        }
+        // END Genesis block mining code
+
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
