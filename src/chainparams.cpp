f7fb57e13bc6c7e51a1d87dc333e1a48fa21a372
Sebastian Rusu <sebastian.rusu@upandrunningsoftware.com>
2017-10-03 19:05:42 +0300

    Updated the pszTimestamp message
    Conflicts:
    	src/chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -48,7 +48,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+    const char* pszTimestamp = "09/06/2017 - Create your own avatar twin that talks like you";
+>>>>>>> e740f04... Updated the pszTimestamp message
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
