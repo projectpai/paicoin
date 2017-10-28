53912363c952301b7fa776ab4d2b0c5e38dc5a62
Sebastian Rusu <sebastian.rusu@upandrunningsoftware.com>
2017-10-28 18:01:50 +0300

    Updated the crypto tests for SHA256.

--- src/test/crypto_tests.cpp
+++ src/test/crypto_tests.cpp
@@ -262,7 +262,7 @@ BOOST_AUTO_TEST_CASE(sha256_testvectors) {
     TestSHA256("This is exactly 64 bytes long, not counting the terminating byte",
                "ab64eff7e88e2e46165e29f2bce41826bd4c7b3552f6b382a9e7d3af47c245f8");
     TestSHA256("As PAIcoin relies on 80 byte header hashes, we want to have an example for that.",
-               "7406e8de7d6e4fffc573daef05aefb8806e7790f55eab5576f31349743cca743");
+               "619cd4e87889698af6b88444bf7d1effbb9cf49257a0a16973c9f0cf17f79572");
     TestSHA256(std::string(1000000, 'a'),
                "cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0");
     TestSHA256(test1, "a316d55510b49662420f49d145d42fb83f31ef8dc016aa4e32df049991a91e26");
