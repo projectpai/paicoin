326ddaf9be3339138c2983fcb699d3e153340c0e
Sebastian Rusu <sebastian.rusu@upandrunningsoftware.com>
2017-11-06 22:15:39 +0200

    Update the Qt unit test.

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -73,7 +73,7 @@ void RPCNestedTests::rpcNestedTests()
     QVERIFY(result == result2);
 
     RPCConsole::RPCExecuteCommandLine(result, "getblock(getbestblockhash())[tx][0]", &filtered);
-    QVERIFY(result == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+    QVERIFY(result == "cb201b1671423313ed67954904a83b4f2dce58aaef70b7748b4ecbd228cacc54");
     QVERIFY(filtered == "getblock(getbestblockhash())[tx][0]");
 
     RPCConsole::RPCParseCommandLine(result, "importprivkey", false, &filtered);
