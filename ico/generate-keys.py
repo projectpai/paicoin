defcc765b218781df455bd7897bbedb49308828d
Sebastian Rusu <sebastian.rusu@upandrunningsoftware.com>
2017-11-07 18:23:56 +0200

    Added methodology to configure and spend the genesis block reward.

--- ico/generate-keys.py
+++ ico/generate-keys.py
@@ -4,5 +4,15 @@ import binascii
 sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
 pk = sk.get_verifying_key()
 
-print("Secret key: " + binascii.hexlify(sk.to_string()).decode('ascii').upper())
-print("Public key: " + binascii.hexlify(pk.to_string()).decode('ascii').upper())
+sk_string = "Secret key: " + binascii.hexlify(sk.to_string()).decode('ascii').lower()
+pk_string = "Public key: 04" + binascii.hexlify(pk.to_string()).decode('ascii').lower()
+
+file = open("keypair.txt", "a")
+file.write("\n")
+file.write(sk_string);
+file.write("\n")
+file.write(pk_string);
+file.write("\n")
+file.close
+
+print("The keypair was generated in the keypair.txt file")
