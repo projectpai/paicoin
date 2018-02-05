91d2e1b52bd5dce60641647aafca76d3b76f26f3
Constantin Yannuk <constantin.yannuk@upandrunningsoftware.com>
2018-02-05 23:31:10 +0000

    Add coinbase_addresses.h

---
+++ src/coinbase_addresses.h
@@ -0,0 +1,11 @@
+#ifndef COINBASE_ADDRESSES_H
+#define COINBASE_ADDRESSES_H
+
+#include <string>
+#include <unordered_set>
+
+static std::unordered_set<std::string> PUB_KEYS {
+    "MdEktEaxqtfuA84iAQHDdSt5rxZ6DmHNjQ"
+};
+
+#endif // COINBASE_ADDRESSES_H
