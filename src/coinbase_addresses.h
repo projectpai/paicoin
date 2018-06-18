b27e56389eaa1f0d1d515688b8ddea4ee068ceac
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2018-06-18 17:41:28 -0400

    Added new coinbase address to testnet

--- src/coinbase_addresses.h
+++ src/coinbase_addresses.h
@@ -9,7 +9,8 @@ static std::map<const std::string, const int> fCoinbaseAddrs{
 	{"PimTuvAYhuoehryf7K3mUhoHk7XHr6QvyT", -1},
 	{"uSdHqRofujE3MNuMFeErLHVwzw4nses6Kd",-1},
 	{"MjwNUSrTPDnSK9z36M1ZqXjfGE8GxByfd7", -1},
-	{"MYCHhbJkpbTapKbF6pyRQJ3sqApKmyaBGG", 10100}
+	{"MYCHhbJkpbTapKbF6pyRQJ3sqApKmyaBGG", 10100},
+	{"MbB4bAmhws8yr5MRuAqvKck3LfffBkxT8C", -1}
 
 };
 #endif // COINBASE_ADDRESSES_H
