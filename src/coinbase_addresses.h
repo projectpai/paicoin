1adf946f4975b77f8d17d7a37beb942d11819be6
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2018-06-19 23:47:29 -0400

    Removed block height restriction due to unexpected startup issues with implementation

--- src/coinbase_addresses.h
+++ src/coinbase_addresses.h
@@ -9,7 +9,7 @@ static std::map<const std::string, const int> fCoinbaseAddrs{
 	{"PimTuvAYhuoehryf7K3mUhoHk7XHr6QvyT", -1},
 	{"uSdHqRofujE3MNuMFeErLHVwzw4nses6Kd",-1},
 	{"MjwNUSrTPDnSK9z36M1ZqXjfGE8GxByfd7", -1},
-	{"MYCHhbJkpbTapKbF6pyRQJ3sqApKmyaBGG", 10100},
+	{"MYCHhbJkpbTapKbF6pyRQJ3sqApKmyaBGG", -1},
 	{"MbB4bAmhws8yr5MRuAqvKck3LfffBkxT8C", -1},
 	{"McvP9ELi7SN4rE2JU5X9E2h7txQyH7nUQd", -1}
 
