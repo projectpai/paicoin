ff88512b7b8a38e3e0bf1477170af8a39642c64d
Mark Harvilla <mark@oben.com>
2018-06-27 08:13:11 -0700

    update generate keys script with input parameter option

--- multisig/generate-keys.py
+++ multisig/generate-keys.py
@@ -4,6 +4,8 @@ import hashlib
 import base58
 import datetime
 import time
+import argparse
+import os.path
 
 # Settings
 
@@ -15,106 +17,119 @@ scriptAddress = "scriptAddress"
 privateAddress = "privateAddress"
 
 base58prefix = {
-	mainnet: {
-		publicAddress: 56,
-		scriptAddress: 130,
-		privateAddress: 247
-	},
+    mainnet: {
+        publicAddress: 56,
+        scriptAddress: 130,
+        privateAddress: 247
+    },
 
-	testnet: {
-		publicAddress: 51,
-		scriptAddress: 180,
-		privateAddress: 226
-	},
+    testnet: {
+        publicAddress: 51,
+        scriptAddress: 180,
+        privateAddress: 226
+    },
 
-	regtest: {
-		publicAddress: 51,
-		scriptAddress: 180,
-		privateAddress: 226
-	} 
+    regtest: {
+        publicAddress: 51,
+        scriptAddress: 180,
+        privateAddress: 226
+    }
 }
 
-# Data generation
 
-d = datetime.datetime.now()
-t = time.mktime(d.timetuple())
+def generate_keys(output_file_path):
+    # Data generation
+    if os.path.exists(output_file_path):
+        print "File already exists. new keypair will be appended to it"
 
-sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-pk = sk.get_verifying_key()
+    d = datetime.datetime.now()
+    t = time.mktime(d.timetuple())
 
-sk_bytes = sk.to_string()
-pk_bytes = "04".decode('hex') + pk.to_string()
+    sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    pk = sk.get_verifying_key()
 
-sk_string = binascii.hexlify(sk_bytes).decode('ascii').lower()
-pk_string = binascii.hexlify(pk_bytes).decode('ascii').lower()
+    sk_bytes = sk.to_string()
+    pk_bytes = "04".decode('hex') + pk.to_string()
 
-addresses = base58prefix
+    sk_string = binascii.hexlify(sk_bytes).decode('ascii').lower()
+    pk_string = binascii.hexlify(pk_bytes).decode('ascii').lower()
 
-for network, addressList in base58prefix.items():
-	for addressType, addressPrefix in addressList.items():
-		if addressType == privateAddress:
-			b = bytes()
-			b += bytes(chr(addressPrefix))
-			b += sk_bytes
+    addresses = base58prefix
 
-			sha256 = hashlib.new("sha256")
-			sha256.update(b)
-			c1 = sha256.digest()
+    for network, addressList in base58prefix.items():
+        for addressType, addressPrefix in addressList.items():
+            if addressType == privateAddress:
+                b = bytes()
+                b += bytes(chr(addressPrefix))
+                b += sk_bytes
 
-			sha256 = hashlib.new("sha256")
-			sha256.update(c1)
-			c2 = sha256.digest()
+                sha256 = hashlib.new("sha256")
+                sha256.update(b)
+                c1 = sha256.digest()
 
-			b += c2[0:4]
+                sha256 = hashlib.new("sha256")
+                sha256.update(c1)
+                c2 = sha256.digest()
 
-			s = base58.b58encode(b)
+                b += c2[0:4]
 
-			addresses[network][addressType] = s
+                s = base58.b58encode(b)
 
-		else:
-			b = bytes()
-			b += bytes(chr(addressPrefix))
+                addresses[network][addressType] = s
 
-			sha256 = hashlib.new("sha256")
-			sha256.update(pk_bytes)
-			p1 = sha256.digest()
+            else:
+                b = bytes()
+                b += bytes(chr(addressPrefix))
 
-			ripemd160 = hashlib.new("ripemd160")
-			ripemd160.update(p1)
-			p2 = ripemd160.digest()
+                sha256 = hashlib.new("sha256")
+                sha256.update(pk_bytes)
+                p1 = sha256.digest()
 
-			b += p2
+                ripemd160 = hashlib.new("ripemd160")
+                ripemd160.update(p1)
+                p2 = ripemd160.digest()
 
-			sha256 = hashlib.new("sha256")
-			sha256.update(b)
-			c1 = sha256.digest()
+                b += p2
 
-			sha256 = hashlib.new("sha256")
-			sha256.update(c1)
-			c2 = sha256.digest()
+                sha256 = hashlib.new("sha256")
+                sha256.update(b)
+                c1 = sha256.digest()
 
-			b += c2[0:4]
+                sha256 = hashlib.new("sha256")
+                sha256.update(c1)
+                c2 = sha256.digest()
 
-			s = base58.b58encode(b)
+                b += c2[0:4]
 
-			addresses[network][addressType] = s
+                s = base58.b58encode(b)
 
-# File output
+                addresses[network][addressType] = s
 
-file = open("keypair.txt", "a")
-file.write("\n\n-------------------------------------------")
-file.write("\nDate: " + str(d))
-file.write("\nTimestamp: " + str(t))
-file.write("\n\nSecret key: " + sk_string)
-file.write("\nPublic key: " + pk_string)
+    # File output
+    with open(output_file_path, "a") as file:
+        file.write("\n\n-------------------------------------------")
+        file.write("\nDate: " + str(d))
+        file.write("\nTimestamp: " + str(t))
+        file.write("\n\nSecret key: " + sk_string)
+        file.write("\nPublic key: " + pk_string)
 
-for network, addressList in addresses.items():
-	file.write("\n\n")
-	file.write(network + ":")
+        for network, addressList in addresses.items():
+            file.write("\n\n")
+            file.write(network + ":")
 
-	for addressType, address in addressList.items():
-		file.write("\n" + addressType + ": " + address)
+            for addressType, address in addressList.items():
+                file.write("\n" + addressType + ": " + str(address))
 
-file.close
+    print "The keypair was generated in the {} file".format(output_file_path)
 
-print("The keypair was generated in the keypair.txt file")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_file_name',
+                        help='Name of the file in which you wish to save your keypair')
+    args = parser.parse_args()
+    generate_keys(args.output_file_name)
+
+
+if __name__ == '__main__':
+    main()
