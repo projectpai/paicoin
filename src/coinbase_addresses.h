b729490b324c4bb6680819c602d9fe4a0cf7ad82
Sebastian Rusu <26344246+sebastianrusu@users.noreply.github.com>
2018-07-02 00:34:37 +0300

    Updated the coinbase address list

--- src/coinbase_addresses.h
+++ src/coinbase_addresses.h
@@ -7,10 +7,12 @@
 static std::map<std::string, int> PUB_KEYS{
     {"2FV2yM5FHytBMU9HLnJgfc83Ud7iw1DbqCK",-1},
     {"PimTuvAYhuoehryf7K3mUhoHk7XHr6QvyT", -1},
-    {"uSdHqRofujE3MNuMFeErLHVwzw4nses6Kd",-1},
+    {"uSdHqRofujE3MNuMFeErLHVwzw4nses6Kd", -1},
     {"MjwNUSrTPDnSK9z36M1ZqXjfGE8GxByfd7", -1},
     {"MYCHhbJkpbTapKbF6pyRQJ3sqApKmyaBGG", -1},
     {"MbB4bAmhws8yr5MRuAqvKck3LfffBkxT8C", -1},
-    {"McvP9ELi7SN4rE2JU5X9E2h7txQyH7nUQd", -1}
+    {"McvP9ELi7SN4rE2JU5X9E2h7txQyH7nUQd", -1},
+   	{"PuS1S2d5WsibDRSXyHynPB5BMRMT7xn7Be", -1},
+	{"PaXVxzkruFZPidQjsDft9CW174Tr99xngu", -1}
 };
 #endif // COINBASE_ADDRESSES_H
