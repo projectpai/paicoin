361f48097026433fcb5d32237a5eb9f0889576b3
Iuri Tovcaci <iuri.tovcaci@upandrunningsoftware.com>
2018-07-06 23:09:55 +0300

    Add lean auxpow merged mining functional tests

--- test/functional/auxpow_attacks.py
+++ test/functional/auxpow_attacks.py
@@ -4,9 +4,14 @@
 """Test AuxPow attacks
 
 - double proof attack
-- self merged mining attack
+- self merged mining attack with parent submitted first and merged
+  mined block second
+- self merged mining attack with merged mined block submitted first
+  and parent block second
 - merged mining auxpow depth check
-- invalid merkle branch"""
+- invalid merkle branch
+
+All the cases are testing both legacy and lean auxpow implementations"""
 
 import copy
 import binascii
@@ -15,7 +20,7 @@ from binascii import b2a_hex
 from decimal import Decimal
 
 from test_framework.blocktools import create_coinbase
-from test_framework.mininode import CBlock, CBlockHeader, CAuxPow, CMerkleTx, hash256, ToHex
+from test_framework.mininode import CBlock, CBlockHeader, CAuxPow, CMerkleTx, hash256, ToHex, uint256_from_str
 from test_framework.test_framework import PAIcoinTestFramework
 
 class AuxPowAttackTest(PAIcoinTestFramework):
@@ -31,10 +36,15 @@ class AuxPowAttackTest(PAIcoinTestFramework):
         # simulate 2 chains/networks for parent/child chain
         self.split_network()
         self.test_doubleProofAttack()
+        self.test_doubleProofAttack_leanAuxpow()
         self.test_selfMergeAttack_Parent_Merged()
+        self.test_selfMergeAttack_Parent_Merged_leanAuxpow()
         self.test_selfMergeAttack_Merged_Parent()
+        self.test_selfMergeAttack_Merged_Parent_leanAuxpow()
         self.test_selfMergeAttack_Parent_Merged_WithDepth()
+        self.test_selfMergeAttack_Parent_Merged_WithDepth_leanAuxpow()
         self.test_invalidMerkleBranch()
+        self.test_invalidMerkleBranch_leanAuxpow()
 
     def test_doubleProofAttack(self):
 
@@ -265,6 +275,202 @@ class AuxPowAttackTest(PAIcoinTestFramework):
         # Sync blocks within their own chains
         self.sync_all([self.nodes[:2], self.nodes[2:]])
 
+    def test_doubleProofAttack_leanAuxpow(self):
+
+        node_child_chain = self.nodes[0]    # child chain network
+        node_parent_chain = self.nodes[2]   # parent chain network
+
+        # Mine a block to leave initial block download
+        node_child_chain.generate(1)
+        child_blk_tmpl = node_child_chain.getblocktemplate()
+
+        # create first child block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(child_blk_tmpl["height"]) + 1)
+        child_block1 = self.createBlockFromTemplate(block_template=child_blk_tmpl, coinbase_tx=coinbase_tx)
+
+        # create second child block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(child_blk_tmpl["height"]) + 2)
+        child_block2 = self.createBlockFromTemplate(block_template=child_blk_tmpl, coinbase_tx=coinbase_tx, hash_prevblock=child_block1.sha256)
+
+        hash_block1 = bytearray(binascii.unhexlify(child_block1.hash))
+        hash_block1.reverse()
+        hash_block2 = bytearray(binascii.unhexlify(child_block2.hash))
+        hash_block2.reverse()
+
+        combined_hash = uint256_from_str(hash256(hash_block1 + hash_block2))
+
+        # create parent block of auxpow attack, solve, submit
+        node_parent_chain.generate(1)
+        parent_blk_tmpl = node_parent_chain.getblocktemplate()
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(parent_blk_tmpl["height"]) + 1)
+
+        parent_block = self.createBlockFromTemplate(block_template=parent_blk_tmpl, coinbase_tx=coinbase_tx)
+        parent_block.auxpowChildrenHash = combined_hash
+        parent_block.solve()
+
+        result = node_parent_chain.submitblock(ToHex(parent_block))
+        assert result is None
+
+        # Now, build valid lean auxpow information to submit to child chain
+        child_block1.auxpow = CAuxPow.fromData(None, [child_block2.sha256], 0, parent_block)
+        result = node_child_chain.submitblock(ToHex(child_block1))
+        assert result is None
+
+        child_block2.auxpow = CAuxPow.fromData(None, [child_block1.sha256], 1, parent_block)
+        result = node_child_chain.submitblock(ToHex(child_block2))
+        assert 'auxpow-double-proof' in result
+
+        # Sync blocks within their own chains
+        self.sync_all([self.nodes[:2], self.nodes[2:]])
+
+    def test_selfMergeAttack_Parent_Merged_leanAuxpow(self):
+
+        node_chain = self.nodes[0]
+
+        # Mine a block to leave initial block download
+        node_chain.generate(1)
+        block_template = node_chain.getblocktemplate()
+
+        # create merged mined block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        merged_mined_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+
+        # create parent block of auxpow attack, include in coinbase the merged mined block hash
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        parent_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+        parent_block.auxpowChildrenHash = merged_mined_block.sha256
+        parent_block.solve()
+
+        result = node_chain.submitblock(ToHex(parent_block))
+        assert result is None
+
+        # Now, build valid auxpow information to try to submit to the chain
+        merged_mined_block.auxpow = CAuxPow.fromData(None, [], 0, parent_block)
+
+        result = node_chain.submitblock(ToHex(merged_mined_block))
+        assert 'auxpow-self' in result
+
+        # Sync blocks within their own chains
+        self.sync_all([self.nodes[:2], self.nodes[2:]])
+
+    def test_selfMergeAttack_Parent_Merged_WithDepth_leanAuxpow(self):
+
+        node_chain = self.nodes[0]
+
+        # Mine a block to leave initial block download
+        node_chain.generate(1)
+        block_template = node_chain.getblocktemplate()
+
+        # create merged mined block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        merged_mined_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+
+        # create parent block of auxpow attack, include in coinbase the merged mined block hash
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        parent_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+        parent_block.auxpowChildrenHash = merged_mined_block.sha256
+        parent_block.solve()
+
+        result = node_chain.submitblock(ToHex(parent_block))
+        assert result is None
+
+        # Now, build valid auxpow information to try to submit to the chain
+        merged_mined_block.auxpow = CAuxPow.fromData(None, [], 0, parent_block)
+
+        node_chain.generate(self.mergedMiningDepthCheck - 1)
+
+        result = node_chain.submitblock(ToHex(merged_mined_block))
+        assert 'auxpow-self' in result
+
+        node_chain.generate(1)
+        result = node_chain.submitblock(ToHex(merged_mined_block))
+        assert 'inconclusive' in result
+
+        # Sync blocks within their own chains
+        self.sync_all([self.nodes[:2], self.nodes[2:]])
+
+    def test_selfMergeAttack_Merged_Parent_leanAuxpow(self):
+
+        node_chain = self.nodes[0]
+
+        # Mine a block to leave initial block download
+        node_chain.generate(1)
+        block_template = node_chain.getblocktemplate()
+
+        # create merged mined block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        merged_mined_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+
+        # create parent block of auxpow attack, include in coinbase the merged mined block hash
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(block_template["height"]) + 1)
+        parent_block = self.createBlockFromTemplate(block_template=block_template, coinbase_tx=coinbase_tx)
+        parent_block.auxpowChildrenHash = merged_mined_block.sha256
+        parent_block.solve()
+
+        # Now, build valid auxpow information to try to submit to the chain
+        merged_mined_block.auxpow = CAuxPow.fromData(None, [], 0, parent_block)
+
+        result = node_chain.submitblock(ToHex(merged_mined_block))
+        assert result is None
+
+        result = node_chain.submitblock(ToHex(parent_block))
+        assert 'auxpow-self' in result
+
+        # Sync blocks within their own chains
+        self.sync_all([self.nodes[:2], self.nodes[2:]])
+
+    def test_invalidMerkleBranch_leanAuxpow(self):
+
+        node_child_chain = self.nodes[0]    # child chain network
+        node_parent_chain = self.nodes[2]   # parent chain network
+
+        # Mine a block to leave initial block download
+        node_child_chain.generate(1)
+        child_blk_tmpl = node_child_chain.getblocktemplate()
+
+        # create first child block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(child_blk_tmpl["height"]) + 1)
+        child_block1 = self.createBlockFromTemplate(block_template=child_blk_tmpl, coinbase_tx=coinbase_tx)
+
+        # create second child block of auxpow attack
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(child_blk_tmpl["height"]) + 2)
+        child_block2 = self.createBlockFromTemplate(block_template=child_blk_tmpl, coinbase_tx=coinbase_tx, hash_prevblock=child_block1.sha256)
+
+        hash_block1 = bytearray(binascii.unhexlify(child_block1.hash))
+        hash_block1.reverse()
+        hash_block2 = bytearray(binascii.unhexlify(child_block2.hash))
+        hash_block2.reverse()
+
+        combined_hash = uint256_from_str(hash256(hash_block1 + hash_block2))
+
+        # create parent block of auxpow attack
+        node_parent_chain.generate(1)
+        parent_blk_tmpl = node_parent_chain.getblocktemplate()
+        coinbase_tx = self.createCoinbaseWithSequence(height=int(parent_blk_tmpl["height"]) + 1)
+
+        parent_block = self.createBlockFromTemplate(block_template=parent_blk_tmpl, coinbase_tx=coinbase_tx)
+        parent_block.auxpowChildrenHash = combined_hash
+        parent_block.solve()
+
+        result = node_parent_chain.submitblock(ToHex(parent_block))
+        assert result is None
+
+        # Now, build valid auxpow information to submit to child chain
+
+        child_block1.auxpow = CAuxPow.fromData(None, [child_block2.sha256], 1, parent_block)
+        result = node_child_chain.submitblock(ToHex(child_block1))
+        assert 'high-hash' in result
+
+        child_block1.auxpow = CAuxPow.fromData(None, [child_block2.sha256], 0, parent_block)
+        result = node_child_chain.submitblock(ToHex(child_block1))
+        assert result is None
+
+        child_block2.auxpow = CAuxPow.fromData(None, [child_block1.sha256], 0, parent_block)
+        result = node_child_chain.submitblock(ToHex(child_block2))
+        assert 'high-hash' in result
+
+        # Sync blocks within their own chains
+        self.sync_all([self.nodes[:2], self.nodes[2:]])
 
     @staticmethod
     def createCoinbaseWithSequence(height=0):
