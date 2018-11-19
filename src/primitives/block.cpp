ec4d586e4c62456fd97f5ce4653383be6d34ec4b
Igor Barjaktarevic <igor.barjaktarevic@upandrunningsoftware.com>
2018-11-19 22:34:49 +0100

    ML Verification support added. WARNING: does not build (linker error)

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -18,12 +18,13 @@ uint256 CBlockHeader::GetHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, powMsgID=%s, powNextMsgID=%s, powModelHash=%s, vtx=%u)\n",
         GetHash().ToString(),
         nVersion,
         hashPrevBlock.ToString(),
         hashMerkleRoot.ToString(),
         nTime, nBits, nNonce,
+        powMsgID, powNextMsgID, powModelHash.ToString(),
         vtx.size());
     for (const auto& tx : vtx) {
         s << "  " << tx->ToString() << "\n";
