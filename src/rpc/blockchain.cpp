7e9aa9ac396f3a10e3a7b345eaffdf555457cebc
Florin Balate <florin.balate@upandrunningsoftware.com>
2019-01-09 19:49:41 +0200

    corrected the previous merge, there were missing changesests resulting in not being able to run the blockchain functional test

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -24,6 +24,7 @@
 #include "util.h"
 #include "utilstrencodings.h"
 #include "hash.h"
+#include "warnings.h"
 
 #include <stdint.h>
 
@@ -1162,6 +1163,7 @@ UniValue getblockchaininfo(const JSONRPCRequest& request)
             "        }\n"
             "     }\n"
             "  }\n"
+            "  \"warnings\" : \"...\",         (string) any network and blockchain warnings.\n"
             "}\n"
             "\nExamples:\n"
             + HelpExampleCli("getblockchaininfo", "")
@@ -1210,6 +1212,7 @@ UniValue getblockchaininfo(const JSONRPCRequest& request)
     obj.push_back(Pair("softforks",             softforks));
     obj.push_back(Pair("bip9_softforks", bip9_softforks));
 
+    obj.push_back(Pair("warnings", GetWarnings("statusbar")));
     return obj;
 }
 
