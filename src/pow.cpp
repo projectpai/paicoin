7722e9e1677cfdc2f17eebadd90eaa5f3df7b74c
Sergey Yezovskikh <sergey.yezovskikh@upandrunningsoftware.com>
2019-08-27 13:28:42 -0500

    added block header parameter to verification protocol

--- src/pow.cpp
+++ src/pow.cpp
@@ -13,7 +13,9 @@
 #include "chain.h"
 #include "util.h"
 #include "primitives/block.h"
+#include "streams.h"
 #include "uint256.h"
+#include "utilstrencodings.h"
 #include "verification_client.h"
 #include "consensus/consensus.h"
 #include "validation.h"
@@ -270,7 +272,10 @@ bool CheckProofOfWork(const CBlockHeader& block, const Consensus::Params& params
     // check ML proof
     std::string verificationServerAddress = gArgs.GetArg("-verificationserver", "localhost:50051");
     VerificationClient client(grpc::CreateChannel(verificationServerAddress, grpc::InsecureChannelCredentials()));
-    auto result = client.Verify(std::string(block.powMsgHistoryId), std::string(block.powMsgId), block.nNonce);
+    CDataStream ssBlock(SER_NETWORK, PROTOCOL_VERSION);
+    ssBlock << block;
+    std::string blockHeaderHex = HexStr(ssBlock.begin(), ssBlock.end());
+    auto result = client.Verify(std::string(block.powMsgHistoryId), std::string(block.powMsgId), block.nNonce, blockHeaderHex);
     int resultCode = int(result.first);
     return resultCode == pai::pouw::verification::Response::OK;
 }
