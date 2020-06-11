7f9502fe27960904fd87ab3f61880e63d0ac2c94
Florin Balate <florin.balate@upandrunningsoftware.com>
2020-06-11 17:51:37 +0300

    * reused old build.sh that builds a deb package first * added service, configmap and statefulset definition in a single deployment yaml

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 4 ]
 then
-    echo "Usage: build.sh <git-user> <git-password> <version>"
+    echo "Usage: build.sh <git-user> <git-password> <version> <branch>"
     exit -1
 fi
 CWD=$(dirname "$(readlink -f $0)") && cd $CWD && \
 docker build --rm -t paicoin-build -f Dockerfile.build . && \
 rm -f *.deb && \
-docker run -e GIT_USER=$1 -e GIT_PASSWD=$2 -e VERSION=$3 --rm --volume $CWD:/paicoin -i -t paicoin-build && \
-docker build --rm -t paicoin:$3 -f Dockerfile .
+docker run -e GIT_USER=$1 -e GIT_PASSWD=$2 -e VERSION=$3 -e BRANCH=$4 --rm --volume $CWD:/paicoin -i -t paicoin-build && \
+docker build --rm -t $4:$3 -f Dockerfile .
