3f8760a3981d1e52c3b82441c630b039b962d1f2
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2020-09-14 17:42:31 -0500

    Reverted min difficulty blocks config.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -319,7 +319,7 @@ public:
         consensus.powLimit = TESTNET_CONSENSUS_POW_LIMIT;
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
-        consensus.fPowAllowMinDifficultyBlocks = false; //Disable for now to rule out weird behavior once mainnet
+        consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1512; // 75% for testchains
         consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
