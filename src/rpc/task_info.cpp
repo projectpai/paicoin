9f7661b27f274fd5dc385abbddadedbce61c5e29
Andrei Lihu <andrei.lihu@aware.gg>
2021-08-26 15:41:24 -0500

    pushed wip code to temporary branch to be able to continue tests and adjustments on an Ubuntu system

---
+++ src/rpc/task_info.cpp
@@ -0,0 +1,49 @@
+#include "rpc/server.h"
+#include "rpc/task_info_client.h"
+
+#include <univalue.h>
+
+
+UniValue getWaitingTasks(const JSONRPCRequest& request)
+{
+    if (request.fHelp || request.params.size() != 2)
+        throw std::runtime_error(
+            "getwaitingtasks ( TaskListRequest )\n"
+
+            "\nArguments:\n"
+            "1. getwaitingtasks_request         (json object) A json object in the following spec\n"
+            "     {\n"
+            "       \"page\":\"page\"       (int, required) Requested page.\n"
+            "       \"per_page\":\"per_page\"           (int, required) Results per page\n"
+            "     }\n"
+            "\n"
+
+            "\nResult:\n"
+            "code     (int) HTTP response code.\n"
+            "pagination     (Pagination) Pagination information.\n"
+            "tasks     ([TaskRecord]) List of tasks.\n"
+        );
+
+    const uint32_t page = request.params[0].get_int();
+    const uint32_t per_page = request.params[1].get_int();
+
+    std::string taskInfoServerAddress = gArgs.GetArg("-verificationserver", "localhost:50051");
+    TaskListClient client(grpc::CreateChannel(taskInfoServerAddress, grpc::InsecureChannelCredentials()));
+    client.TestGetWaitingTasks();
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("test", page));
+    return obj;
+}
+
+static const CRPCCommand commands[] =
+{ //  category              name                      actor (function)         argNames
+  //  --------------------- ------------------------  -----------------------  ----------
+    { "task_info",             "getwaitingtasks",       &getWaitingTasks,       {"page","per_page"} },
+};
+
+void RegisterMiningRPCCommands(CRPCTable &t)
+{
+    for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
+        t.appendCommand(commands[vcidx].name, &commands[vcidx]);
+}
